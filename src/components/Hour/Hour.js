82365d92dfe4de7d268dad5d451a8f999c0c3fe0
Bekzod Raxmonov <raxmonovb215@gmail.com>
2021-04-12 12:22:20 +0500

    gettime

--- src/components/Hour/Hour.js
+++ src/components/Hour/Hour.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './Hour.scss'
 
 const Hour = () => {
+    const [time,setTime] = useState({second: "",minute: ""})
+    function Gettime() {
+     setInterval(() => {
+        let cleartime = new Date();
+
+    setTime({
+      second: cleartime.getHours(),
+      minute: cleartime.getMinutes()
+    })
+     },1000);
+   }
+
+  Gettime()
+
     return (
-        <> 
+        <>
           <p>
-              <span>09:41</span>| Tashkent.
+             <span>Current time:{time.second}:{time.minute}</span>
+             <span></span>
           </p>
         </>
     )
