10b8a5cc2b29f823c1a1c027fac6e91d99324c8a
sherzod38 <72493786+sherzod38@users.noreply.github.com>
2021-04-15 12:08:29 +0500

    f9

--- src/components/BookCard/Item.jsx
+++ src/components/BookCard/Item.jsx
@@ -12,7 +12,7 @@ const Item = ({
 }) => {
   return (
     <div className="book-card container" key={id}>
-      <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt="" />
+      <img className='book-card-img' src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt="" />
       <h3 className="book-card-name">{title}</h3>
       <p className="book-card-views">
         {release_date ? "Muallifi" : release_date}
