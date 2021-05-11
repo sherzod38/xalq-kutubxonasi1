69579ee66cc3bc03780ac6ad5cf9de2feae76276
sherzod38 <72493786+sherzod38@users.noreply.github.com>
2021-05-11 16:08:19 +0500

    f47

--- src/components/Language/Language.js
+++ src/components/Language/Language.js
@@ -1,18 +1,30 @@
-import './Language.scss';
-import AuthContext from "../../context/Auth/context";
-import { useContext } from 'react';
 
-const Language = () => {
-    const {methods} = useContext(AuthContext)
-    function langCanger(e) {
-        methods.setLang(e.target.value)
-        localStorage.setItem("lang", e.target.value)
+import './Language.scss';
+// import AuthContext from "../../context/Auth/context";
+// import { useContext } from 'react';
+
+const Language = (active) => {
+    // const {methods} = useContext(AuthContext)
+    // function langCanger(e) {
+    //     methods.setLang(e.target.value)
+    //     localStorage.setItem("lang", e.target.value)
+    // }
+
+    
+    const handleclik = () => {
+
     }
     return (
-        <select className="language" onChange={langCanger}>
-           <option value="Lotin">Lotin</option>
-           < option value="Krill"> Крилл </option>
-        </select>
+        // <select className="language" onChange={langCanger}>
+        //    <option value="Lotin">Lotin</option>
+        //    < option value="Krill"> Крилл </option>
+        // </select>
+        
+        <div className="language">
+            <button onClick={handleclik} className={`language-lotin ${active ? 'active' : ''}`}>Lotin</button>
+            <span className="language-span">|</span>
+            <button onClick={handleclik} className={`language-kiril ${active ? '' : 'active'}`}>Кирил</button>
+        </div>
     )
 }
 
